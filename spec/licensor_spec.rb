1c3f8218144e65c62003432a3df370e7a26fc734
OTSUKA Tatsuya <tatzyr@gmail.com>
2015-06-28 17:55:52 +0900

    Switch from minitest to RSpec

---
+++ spec/licensor_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Licensor do
+  it 'has a version number' do
+    expect(Licensor::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
